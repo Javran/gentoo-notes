e87f5cecd9607c006cb599fb57a9c12ccc37918e
Javran Cheng <Javran.c@gmail.com>
2014-01-15 20:24:50 -0500

    new way of module rebuilding

--- kernel-upgrade.sh
+++ kernel-upgrade.sh
@@ -21,3 +21,15 @@ make -j3 && make install && make modules_install && module-rebuild rebuild
 
 # don't forget to update boot menu.
 vim /boot/grub/grub.conf
+
+# also need to rebuild modules
+
+# in old versions:
+# populate is not required everytime as long as
+#   it is not the first time or no module related packages are newly installed
+module-rebuild populate
+module-rebuild rebuild
+
+# in new version, seems we need no longer module-rebuild
+
+emerge @module-rebuild
